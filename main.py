dd4a51775400c674bd4b013c5604e56c7cce8daf
karunkarthik-git <kkarun0542@gmail.com>
2025-06-30 21:06:39 +0530

    Added procfile

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from routes.user_router import router as user_router
 from routes.post_router import router as post_router
 from routes.comment_router import router as comment_router
-
+import os
 from middleware import AuthMiddleware
 
 app = FastAPI(
@@ -20,3 +20,8 @@ app.add_middleware(AuthMiddleware)
 app.include_router(user_router, prefix="/api/v1/users", tags=["users"])
 app.include_router(post_router, prefix="/api/v1/posts", tags=["posts"])
 app.include_router(comment_router, prefix="/api/v1/comments", tags=["comments"])
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
